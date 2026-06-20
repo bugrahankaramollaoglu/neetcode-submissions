404190cdd3c775e9b2becbd9d41cdb6bd67b1cae
Buğrahan Karamollaoğlu <bugrahankaramollaoglu@gmail.com>
2026-06-20 22:10:34 +0300

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    // brute force O(n^2)
+
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+}
